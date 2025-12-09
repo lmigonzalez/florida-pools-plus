59ff19dda0321d55223ef5b099d34baaaab14dce
lmigonzalez <lmigonzalezs@gmail.com>
2025-12-09 10:25:13 -0500

    first commit

---
+++ src/aos.d.ts
@@ -0,0 +1,19 @@
+declare module "aos" {
+  interface AOSOptions {
+    duration?: number;
+    easing?: string;
+    once?: boolean;
+    offset?: number;
+    delay?: number;
+  }
+
+  interface AOS {
+    init(options?: AOSOptions): void;
+    refresh(): void;
+    refreshHard(): void;
+  }
+
+  const AOS: AOS;
+  export default AOS;
+}
+
